2761f5892eb7ad6dcf853339f307d1f5adb07148
bryankahlil <76204588+bryankahlil@users.noreply.github.com>
2024-06-09 20:02:17 +0800

    Add files via upload

--- BE/index.js
+++ BE/index.js
@@ -3,17 +3,19 @@ import cors from 'cors';
 import userRoutes from './routes/routes.js';
 import dotenv from 'dotenv';
 dotenv.config();
+
 console.log('host is index in ' + process.env.DB_HOST);
 
-const port = process.env.PORT
+const port = process.env.PORT || 3001; // Ensure a default port is provided
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
-app.use('/api', userRoutes);
 
+// Routes
+app.use('/api', userRoutes);
 
 app.listen(port, () => {
   console.log('Server running on port ' + port);
