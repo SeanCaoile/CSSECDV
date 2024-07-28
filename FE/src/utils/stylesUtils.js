1102709d4a7c8f35ed309bab6ac4b49f98a80618
bryankahlil <76204588+bryankahlil@users.noreply.github.com>
2024-07-28 22:17:04 +0800

    Add files via upload

--- FE/src/utils/stylesUtils.js
+++ FE/src/utils/stylesUtils.js
@@ -1,19 +1,39 @@
 // utils/styleUtils.js
 export function resetAppStyles() {
-    const appElement = document.getElementById('app');
-    if (appElement) {
-      appElement.style.display = 'grid';
-      appElement.style.gridTemplateColumns = '1fr 1fr';
-      appElement.style.padding = '0 2rem';
-    }
+  const appElement = document.getElementById('app');
+  if (appElement) {
+    appElement.style.display = 'grid';
+    appElement.style.gridTemplateColumns = '1fr 1fr';
+    appElement.style.padding = '0 2rem';
   }
-  
-  export function setAppStylesForHome() {
-    const appElement = document.getElementById('app');
-    if (appElement) {
-      appElement.style.display = 'block';
-      appElement.style.gridTemplateColumns = '';
-      appElement.style.padding = '';
-    }
+}
+
+export function setAppStylesForHome() {
+  const appElement = document.getElementById('app');
+  if (appElement) {
+    appElement.style.display = 'block';
+    appElement.style.gridTemplateColumns = '';
+    appElement.style.padding = '';
   }
-  
+}
+
+export function setAppStylesForEditBlog() {
+  const appElement = document.getElementById('app');
+  if (appElement) {
+    appElement.style.display = 'flex';
+    appElement.style.justifyContent = 'center';
+    appElement.style.alignItems = 'center';
+    appElement.style.height = '100vh'; // Ensure full viewport height
+    appElement.style.padding = '0';
+  }
+}
+
+export function setAppStylesForBlogDetail() {
+  const appElement = document.getElementById('app');
+  if (appElement) {
+    appElement.style.display = 'flex';
+    appElement.style.justifyContent = 'center';
+    appElement.style.alignItems = 'flex-start'; // Align items to the start of the container
+    appElement.style.padding = '2rem'; // Adjust padding if needed
+  }
+}
