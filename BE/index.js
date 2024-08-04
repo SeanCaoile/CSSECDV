1f0f1dbe1b806f953049ba530d444bef25b2baed
SeanCaoile <sean_iverson_caoile@dlsu.edu.ph>
2024-08-05 04:33:16 +0800

    Update index.js

--- BE/index.js
+++ BE/index.js
@@ -20,8 +20,8 @@ const port = process.env.PORT;
 const app = express();
 
 // Increase the limit for JSON payloads
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use(cors({
